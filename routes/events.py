3d3324b31c199c0d217a1f64a5dda50b645e4e10
devil206 <bhardwajshivansh206@gmail.com>
2025-07-15 23:49:45 +0530

    Initial commit

---
+++ routes/events.py
@@ -0,0 +1,58 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from models.events import Event
+from database.connection import get_session
+from models.events import Event, EventUpdate
+from sqlmodel import select
+
+event_router = APIRouter(
+    tags=["Events"]
+)
+
+events = []
+
+@event_router.get("/", response_model=List[Event])
+async def retrieve_all_events(session = Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).all()
+    return events
+
+
+@event_router.get("/{id}", response_model=Event)
+async def retrieve_event(id: int, session = Depends(get_session)) -> Event:
+        event = session.get(Event, id)
+        if event:
+            return event
+        
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event with supplied ID does not exist"
+         )
+
+
+
+@event_router.post("/new")
+async def create_event(new_event: Event,
+session = Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+    return {
+        "message": "Event created successfully"
+    }
+
+
+@event_router.delete("/{id}")
+async def delete_event(id: int) -> dict:
+    for event in events:
+
+        if event.id == id:
+            events.remove(event)
+
+            return {
+                "message": "Event deleted successfully."
+                }
+    raise HTTPException(
+        status_code= status.HTTP_404_NOT_FOUND,
+        detail = "Event with the supplied Id does not exist"
+    )
